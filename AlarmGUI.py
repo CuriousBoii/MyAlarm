501063aa97f742a4fa3be13be9e2c8f97a9772ed
thinkpad <2022an0120778@gmail.com>
2022-08-07 22:35:09 +0530

    just specified a root for the Alarm

---
+++ AlarmGUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+root = Tk()
+root.title("MyAlarm App")
+root.geometry("550x600")
+
+entry1 = Entry()
+entry1.pack()
+
+labl = Label(root, text="hi !!")
+print("this is from the idle")
+print("this is from vscode")
