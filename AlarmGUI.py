d3c78fe5a83a3f508016d5908db606a78940a404
vishnu <tavs713@gmail.com>
2022-08-20 09:30:40 +0530

    textfields in gui

--- AlarmGUI.py
+++ AlarmGUI.py
@@ -1,20 +1,19 @@
-from cProfile import label
-from cmd import PROMPT
+
 from time import *
 from Main import *
 from tkinter import *
+#bot = AlarmRinger(list1)
 root = Tk()
-root.geometry("500x150")
-b = ctime()
+root.geometry("500x100")
 a = Label(root, text = ctime(), fg = "black", bg = "red")
 a.pack()
-enterTime = Entry()
-enterTime.place(x=0,y=0)
-enterDate = Entry()
-enterDate.place(x=375,y=0)
-enterText = Entry()
-enterText.place(x=186,y=43)
-b = Label(text="boom")
+enterTime = Entry(root)
+enterTime.place(relx=0,y=0)
+enterDate = Entry(root)
+enterDate.place(relx=0.75,rely=0)
+enterText = Entry(root)
+enterText.place(relx=0.373,rely=0.5)
+b = Label(root, text="boom")
 b.pack()
 
 root.mainloop()
